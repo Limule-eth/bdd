dc5c2ca0e2d4a94100db201abff33411b22e2c7e
Limule-eth <nicolas.klaine@sfr.fr>
2023-06-11 08:10:16 +0200

    base

---
+++ home.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Base de donnée</title>
+	<meta name="description" content="Base de donnée">
+	<link rel="stylesheet" type="text/css" href="../../css/bdd.css">
+	<link rel="stylesheet" type="text/css" href="../../css/0css.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Alkatra&family=Bitter&family=Open+Sans&family=Overlock&family=Rubik+Marker+Hatch&display=swap" rel="stylesheet"> 
+</head>
+
+<body>
+
+
+<!--================================================================================================================================================================-->
+<!--================================================================================================================================================================-->
+<!--============================================================================================================================================ BODY - MAIN =======-->
+
+
+<main>
+
+	<h1>- Base de donnée -</h1>
+
+	<section class="bulle mar">
+		<h2>PHP</h2>
+
+			<ul>
+				<li><a href="">lien 1</a></li>
+			</ul>
+
+	</section>
+
+</main>
+
+
+<!--================================================================================================================================================================-->
+<!--================================================================================================================================================================-->
+<!--============================================================================================================================================ BODY - FOOTER =====-->
+	
+
+		
+</body>
+</html>
